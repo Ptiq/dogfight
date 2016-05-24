4736fcd6e595ff1f9933c34754a71bf860860fcc
Yann <Yann@10.133.50.9>
2016-05-24 10:25:19 +0200

    Phase 2 Yannou

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,29 @@
+package jpu2016.dogfight.controller;
+
+public class DogfightController {
+	private static int TIME_SLEEP = 30;
+
+	public DogfightController() {
+
+	}
+
+	private void gameLoop() {
+
+	}
+
+	private void lauchMissile() {
+
+	}
+
+	public void orderPerform() {
+
+	}
+
+	public void play() {
+
+	}
+
+	public void setViewSystem() {
+
+	}
+}
---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,17 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	private int player;
+
+	public void getOrder() {
+
+	}
+
+	public void getPlayer() {
+
+	}
+
+	public void UserOrder() {
+
+	}
+}

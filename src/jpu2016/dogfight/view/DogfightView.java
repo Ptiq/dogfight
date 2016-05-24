13fa4a3b08549e0f3c32290ce24332cc9beecdab
BenjaminParageaud <benjitoon33@gmail.com>
2016-05-24 10:13:45 +0200

    lel*

---
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.view;
+
+public class DogfightView {
+
+	public DogfightView(){
+
+	}
+
+	public void closeAll(){
+
+	}
+
+	public void displayMessage(){
+
+	}
+
+	public void run(){
+
+	}
+}
